9a56375febbbe8f0369e6bc87c7b6696cff69400
ahirina rivera <ahirina.rivera@verizon.com>
2019-10-02 18:12:01 -0500

    Refactor to Eliminate Status Code

--- routes/courses.js
+++ routes/courses.js
@@ -3,16 +3,16 @@ const router = express.Router();
 const { Course, User } = require('../models');
 
 //Returns a list of courses (includin the user that owns this course)
-router.get('/', async (req,res) => {
+router.get('/', async (req, res) => {
     const courses = await Course.findAll({ include: [ User ] });
-    res.status(200).json(courses);
+    res.json(courses);
 });
 
 //Returns a course (including the user that owns the course) for the provided course ID
 router.get('/:id', async (req, res) => {
     const id = req.params.id;
     const course = await Course.findByPk(id, { include: [ User ] });
-    res.status(200).json(course);
+    res.json(course);
 });
 
 //Creates a course 
